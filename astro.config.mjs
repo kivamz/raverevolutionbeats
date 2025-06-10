f6122f1d8f7d823e5e28626035aa84e88dd5b30b
Kevin Marín Zamarrón <kmarin@toools.es>
2025-06-10 21:41:24 +0200

    novedades

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: process.env.NODE_ENV === 'production'
+    ? 'https://github.com/kivamz/raverevolutionbeats/'
+    : 'http://localhost:4321',
   base: process.env.NODE_ENV === 'production' ? '/raverevolutionbeats/' : '/',
+  vite: {
+    define: {
+      // Solo se expone al cliente información no sensible
+      'import.meta.env.SITE': JSON.stringify(process.env.NODE_ENV === 'production'
+        ? 'https://github.com/kivamz/raverevolutionbeats/'
+        : 'http://localhost:4321')
+    }
+  }
 });
